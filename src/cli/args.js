a6c731c97f210ae9d4149a7282c579748c7e5ee2
Tanya Samal <solesytto@gmail.com>
2022-05-31 23:24:41 +0300

    feat: solve cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,19 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let strResult = '';
+
+    process.argv.forEach((arg) => {
+        if (arg.startsWith('--') || arg.startsWith('-')) {
+            const argName = arg.replace(/-{1,2}/g, '');
+            strResult = strResult.concat(`${argName} is ${getArg(arg)}, `);
+        }
+    });
+
+    console.log(strResult.slice(0, -2));
 };
+
+function getArg(argName) {
+    const argIndex = process.argv.indexOf(argName);
+    return argIndex !== -1 ? process.argv[argIndex + 1] : null;
+}
+
+parseArgs();
