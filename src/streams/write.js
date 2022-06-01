5810cc9386590a3639611f1e971e3b39bf4f6794
Tanya Samal <solesytto@gmail.com>
2022-06-01 20:37:23 +0300

    feat: solve streams tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const write = async () => {
-    // Write your code here 
+    const file = join(__dirname, 'files', 'fileToWrite.txt');
+    const output = createWriteStream(file, 'utf-8');
+
+    process.stdin.on('data', data => output.write(data));
 };
+
+write();
