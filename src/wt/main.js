7a5015ed260c1cb286b9fa65bdd4cbf90d21b984
Tanya Samal <solesytto@gmail.com>
2022-06-02 15:14:54 +0300

    feat: solve wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,40 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const workerFile = join(__dirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+
+    for(let i = 0; i < cpus().length; i += 1) {
+        promises.push(getPromiseFromWorker(i));
+    }
+
+    const promiseResults = await Promise.allSettled(promises);
+    const resultArr = [];
+
+    promiseResults.forEach((result) => {
+        resultArr.push({
+            status : result.status === 'fulfilled' ? 'resolved' : 'error',
+            data: result.status === 'fulfilled' ? result.value : null,
+        })
+    });
+
+    console.log(resultArr);
 };
+
+const getPromiseFromWorker = (n) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(workerFile, { 
+            workerData: 10 + n,
+        });
+        worker.on('message', (msg) => resolve(msg));
+        worker.on('error', reject);
+    });
+}
+
+performCalculations();
