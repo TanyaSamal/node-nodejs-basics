a118e16206dce64d4afd084a5ec78b1d4ab342aa
Tanya Samal <solesytto@gmail.com>
2022-06-03 19:57:12 +0300

    feat: solve zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import { pipeline }  from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const compress = async () => {
-    // Write your code here 
+    const fileToCompress = join(__dirname, 'files', 'fileToCompress.txt');
+    const archive = join(__dirname, 'files', 'archive.gz');
+
+    try {
+        await pipeline(
+            createReadStream(fileToCompress),
+            createGzip(),
+            createWriteStream(archive)
+        );
+    } catch (err) {
+        if (err) throw new Error('Operation failed');
+    }
 };
+
+compress();
